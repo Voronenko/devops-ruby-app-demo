a74f3fe330435fa7bbfbc80fd1c32baf27b0fbde
Vyacheslav <voronenko@gmail.com>
2016-09-15 13:51:39 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,5 @@
 
 sudo apt-get install python-pip
 pip install -U pip
-sudo -H pip install ansible==1.9.4
-sudo -H pip install prudentia==1.0
+sudo -H pip install ansible
+sudo -H pip install prudentia
